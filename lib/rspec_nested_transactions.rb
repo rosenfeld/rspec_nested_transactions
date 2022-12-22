302bc6926450e042491554a0f5515457692374ac
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2022-12-21 23:55:29 -0300

    Add support for newer ActiveRecord versions

--- lib/rspec_nested_transactions.rb
+++ lib/rspec_nested_transactions.rb
@@ -20,9 +20,9 @@ module RspecNestedTransactions
     end
   end
 
-  def nested_transaction(&block)
+  def nested_transaction(separate_thread: true, &block)
     around &wrapped_block(&block)
-    nested_transaction_contexts(&block)
+    nested_transaction_contexts(separate_thread, &block)
   end
 
   private
@@ -33,27 +33,41 @@ module RspecNestedTransactions
     ->(c){ yield c, ->(*args){ c.respond_to?(:run) ? c.run : c.run_examples } }
   end
 
-  def nested_transaction_contexts(&block)
-    (handle_config_around &block; return) if ::RSpec::Core::Configuration === self
+  def nested_transaction_contexts(separate_thread, &block)
+    (handle_config_around separate_thread, &block; return) if ::RSpec::Core::Configuration === self
     around_all(false) do |group|
-      group.children.each {|c| c.send :nested_transaction_contexts, &block }
+      group.children.each {|c| c.send :nested_transaction_contexts, separate_thread, &block }
       block[ group, ->(*args){ group.run_examples } ]
     end
   end
 
   CONFIG_AROUND_ALL_NESTED_BLOCKS = []
   CONFIG_AROUND_ALL_PROCESSED_BY_GROUP = {}
-  def handle_config_around(store = true, prepend = false, &block)
+  def handle_config_around(separate_thread, store = true, prepend = false, &block)
     blocks = CONFIG_AROUND_ALL_NESTED_BLOCKS
     blocks << block if store
     around_all(prepend) do |group|
       unless CONFIG_AROUND_ALL_PROCESSED_BY_GROUP[group.name]
         CONFIG_AROUND_ALL_PROCESSED_BY_GROUP[group.name] = true
         blocks.reverse_each do |b|
-          group.children.each{|c| c.send :handle_config_around, false, true, &b }
+          group.children.each{|c| c.send :handle_config_around, separate_thread, false, true, &b }
         end
       end
-      block[ group, ->(*args){group.run_examples } ]
+      # ActiveRecord tries to get an exclusive lock for the thread that would be owned by
+      # the fiber if we don't spawn a separate thread for controlling the execution of the
+      # block. To handle cases like that, we use a separate thread by default.
+      if !separate_thread
+        block[ group, ->(*args){ group.run_examples } ]
+      else
+        q1, q2 = Queue.new, Queue.new
+        thread = Thread.start do
+          block[ group, ->(*args) { q2 << 1; q1.pop }]
+        end
+        q2.pop
+        group.run_examples
+        q1 << 1
+        thread.join
+      end
     end
   end
 
